fff01203eb863043dca4ecb087191f06d2d6cd79
ChanSeok Park <ict.cspark@gmail.com>
2023-03-29 14:39:48 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    
+    while n > 1:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                answer.append(i)
+                n = n // i
+                break
+             
+    answer = sorted(list(set(answer)))
+    return answer
