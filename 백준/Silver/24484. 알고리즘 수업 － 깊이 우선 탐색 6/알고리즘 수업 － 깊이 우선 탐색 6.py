547f52b3b7f338f5297054138887037f78b8156c
ChanSeok Park <ict.cspark@gmail.com>
2023-04-04 00:13:36 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 6, Time: 636 ms, Memory: 81796 KB -BaekjoonHub

--- 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.py
+++ 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.py
@@ -1,39 +1,41 @@
 import sys
-sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-def DFS(r):
-    global flag
-    G = sorted(graph[r], reverse=True)
-    for g in G:
-        if visited[g] == 0:
-            visited[g] = 1
-            flag += 1
-            T[g] = flag
-            D[g] = D[r] + 1
-            DFS(g)
-
-
 N, M, R = map(int, input().split())
-
 graph = [[] for _ in range(N + 1)]
+
 for _ in range(M):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-visited = [0] * (N + 1)
-T = [0] * (N + 1)
+
+def DFS(r):
+    global flag
+    idx = sorted(graph[r], reverse=True)
+    for i in idx:
+        if visited[i] == 0:
+            visited[i] = 1
+            flag += 1
+            T[i] = flag
+            D[i] = D[r] + 1
+            DFS(i)
+            
+
 D = [-1] * (N + 1)
+T = [0] * (N + 1)
+visited = [0] * (N + 1)
+
+D[R] = 0
+T[R] = 1
+visited[R] = 1
 
 flag = 1
-visited[R] = 1
-T[R] = flag
-D[R] = 0
 DFS(R)
 
 result = 0
-for i in range(1, N + 1):
-    result += T[i] * D[i]
+for j in range(1, N + 1):
+    result += D[j] * T[j]
 
 print(result)
