d6cc433ca40aad6cd1c84a6fe2e480061351bcc3
ChanSeok Park <ict.cspark@gmail.com>
2023-04-11 16:54:56 +0900

    [Silver V] Title: 단어 정렬, Time: 808 ms, Memory: 35032 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+words = set()
+
+for _ in range(N):
+    word = input()
+    words.add(word)
+
+words = sorted(list(words))
+result = sorted(words, key=lambda x: len(x))
+
+for r in result:
+    print(r)
