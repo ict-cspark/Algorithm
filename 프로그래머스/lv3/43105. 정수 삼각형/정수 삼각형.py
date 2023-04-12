b0805df823c2fd1be389b7f3dbd32e7281378422
ChanSeok Park <ict.cspark@gmail.com>
2023-04-12 17:37:14 +0900

    [level 3] Title: 정수 삼각형, Time: 54.16 ms, Memory: 14.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,14 @@
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            left = 0
+            right = 0
+            if 0 <= j - 1:
+                left = triangle[i - 1][j - 1]
+            if j < len(triangle[i - 1]):
+                right = triangle[i - 1][j]
+
+            triangle[i][j] += max(left, right)
+
+    answer = max(triangle[-1])
+    return answer
