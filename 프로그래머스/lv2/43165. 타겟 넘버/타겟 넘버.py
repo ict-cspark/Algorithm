3d89f3912d6c39f7ad1451ee32df397450e926b3
ChanSeok Park <ict.cspark@gmail.com>
2023-04-12 17:39:34 +0900

    [level 2] Title: 타겟 넘버, Time: 2.28 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,21 @@
+def solution(numbers, target):
+    answer = 0
+    num_len = len(numbers)
+
+    def DFS(N, K):
+        nonlocal answer
+
+        if N == num_len:
+            if K == target:
+                answer += 1
+                return
+
+        else:
+            DFS(N + 1, K + numbers[N])
+            DFS(N + 1, K - numbers[N])
+
+        return
+
+    DFS(0, 0)
+
+    return answer
