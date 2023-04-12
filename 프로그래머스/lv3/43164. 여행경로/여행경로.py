a528ee2509742c8a2a58cd9b97434d222187362a
ChanSeok Park <ict.cspark@gmail.com>
2023-04-12 17:36:51 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+
+def solution(tickets):
+    answer = []
+    routes = defaultdict(list)
+    for start, end in tickets:
+        routes[start].append(end)
+
+    print(routes)
+    for route in routes:
+        routes[route].sort(reverse=True)
+
+    stack = ["ICN"]
+    while stack:
+        plane = stack.pop()
+        if plane not in routes or not routes[plane]:
+            answer.append(plane)
+        else:
+            stack.append(plane)
+            stack.append(routes[plane].pop())
+
+    return answer[::-1]
