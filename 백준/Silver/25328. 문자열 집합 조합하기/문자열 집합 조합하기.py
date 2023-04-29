c7b9bebbca76cabbe1e5196eedcac27c2bfe5fd8
ChanSeok Park <ict.cspark@gmail.com>
2023-04-29 10:39:53 +0900

    [Silver III] Title: 문자열 집합 조합하기, Time: 192 ms, Memory: 122460 KB -BaekjoonHub

--- 백준/Silver/25328. 문자열 집합 조합하기/문자열 집합 조합하기.py
+++ 백준/Silver/25328. 문자열 집합 조합하기/문자열 집합 조합하기.py
@@ -5,19 +5,20 @@ input = sys.stdin.readline
 words = [input().strip() for _ in range(3)]
 k = int(input())
 
-temp = []
-result = set()
+temp = dict()
+result = dict()
 for word in words:
     for w in combinations(word, k):
         answer = "".join(w)
         if answer in temp:
-            result.add(answer)
+            result[answer] = 1
         else:
-            temp.append(answer)
+            temp[answer] = 1
 
-result = sorted(list(result))
+
 if result:
+    result = sorted(result.items())
     for r in result:
-        print(r)
+        print(r[0])
 else:
     print(-1)
