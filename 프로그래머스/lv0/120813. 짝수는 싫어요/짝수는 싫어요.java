f905735d1f9774ed04bd717c49e5057aa749faf6
ChanSeok Park <ict.cspark@gmail.com>
2023-04-30 18:44:47 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer;
+        
+        if (n % 2 == 1) {
+            answer = new int[(n / 2) + 1];
+        } else {
+            answer = new int[n / 2];
+        }
+        
+        int idx = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 1) {
+                answer[idx] = i;
+                idx += 1;
+            }
+        }
+        return answer;
+    }
+}
