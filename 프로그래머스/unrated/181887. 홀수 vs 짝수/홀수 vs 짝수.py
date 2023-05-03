0765469e883bb9a67239301b490570ea7d53101e
ChanSeok Park <ict.cspark@gmail.com>
2023-05-03 18:19:08 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    odd = 0
+    even = 0
+    for i in range(len(num_list)):
+        if i % 2:
+            even += num_list[i]
+        else:
+            odd += num_list[i]
+            
+    answer = max(odd, even)
+    return answer
