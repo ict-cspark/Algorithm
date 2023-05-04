93741f4314822c6c8f41351ac3b1f30b1ecc3fc8
ChanSeok Park <ict.cspark@gmail.com>
2023-05-04 11:46:22 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 17.88 ms, Memory: 15.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    for i in range(len(arr)):
+        num = arr[i]
+        if num >= 50 and num % 2 == 0:
+            arr[i] = num // 2
+        elif num < 50 and num % 2:
+            arr[i] = num * 2
+    return arr
