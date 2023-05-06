7f03549800ae8ba40363a3607bbc9adc99e4f7c7
ChanSeok Park <ict.cspark@gmail.com>
2023-05-06 21:22:47 +0900

    [Silver III] Title: 정식당, Time: 280 ms, Memory: 50524 KB -BaekjoonHub

---
+++ 백준/Silver/17479. 정식당/정식당.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+A, B, C = map(int, input().split())
+
+mainMenu = dict()
+specialMenu = dict()
+serviceMenu = dict()
+
+for _ in range(A):
+    menu, price = map(str, input().split())
+    mainMenu[menu] = int(price)
+
+for _ in range(B):
+    menu, price = map(str, input().split())
+    specialMenu[menu] = int(price)
+
+for _ in range(C):
+    menu = input().strip()
+    serviceMenu[menu] = 0
+
+mainTotal = 0
+specialTotal = 0
+serviceCnt = 0
+N = int(input())
+for _ in range(N):
+    menu = input().strip()
+    if menu in mainMenu:
+        mainTotal += mainMenu[menu]
+    elif menu in specialMenu:
+        specialTotal += specialMenu[menu]
+    else:
+        serviceCnt += 1
+
+result = "Okay"
+if mainTotal < 20000 and specialTotal > 0:
+    result = 'No'
+elif mainTotal + specialTotal < 50000 and serviceCnt > 0:
+    result = 'No'
+elif serviceCnt > 1:
+    result = 'No'
+
+print(result)
