22163085edf069581c71d4faa5b8cd29466acc1c
ChanSeok Park <ict.cspark@gmail.com>
2023-06-08 23:10:48 +0900

    [Silver I] Title: 망가진 키보드, Time: 3696 ms, Memory: 34460 KB -BaekjoonHub

--- 백준/Silver/6503. 망가진 키보드/망가진 키보드.py
+++ 백준/Silver/6503. 망가진 키보드/망가진 키보드.py
@@ -5,13 +5,13 @@ while True:
     M = int(input())
     if M == 0:
         break
-    sentence = input().strip()
-    N = len(sentence)
+    words = input().strip()
+
+    N = len(words)
+    M_dic = dict()
+    M_dic[words[0]] = 1
 
     result = 0
-    M_dic = dict()
-    M_dic[sentence[0]] = 1
-
     left = 0
     right = 0
     while right < N:
@@ -19,18 +19,17 @@ while True:
             result = max(result, right - left + 1)
             right += 1
             if right < N:
-                alph = sentence[right]
+                alph = words[right]
                 if alph in M_dic:
                     M_dic[alph] += 1
                 else:
                     M_dic[alph] = 1
 
         else:
-            alph = sentence[left]
-            if alph in M_dic:
-                M_dic[alph] -= 1
-                if M_dic[alph] == 0:
-                    del M_dic[alph]
-                left += 1
+            alph = words[left]
+            M_dic[alph] -= 1
+            if M_dic[alph] == 0:
+                del M_dic[alph]
+            left += 1
 
     print(result)
