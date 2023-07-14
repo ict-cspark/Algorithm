92a5c835cbd2920defc67bed15906a7bbfa0969b
ChanSeok Park <ict.cspark@gmail.com>
2023-07-15 08:01:37 +0900

    Update 타노스.py

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,15 +1,19 @@
 N = input()
 
-even = 0
-odd = 0
-for n in N:
-    if int(n):
-        odd += 1
-    else:
-        even += 1
+odd = N.count("1") // 2
+even = N.count("0") // 2
 
-even = even // 2
-odd = odd // 2
+N = list(N)
+for _ in range(odd):
+    idx = N.index("1")
+    N.pop(idx)
 
-result = "0" * even + "1" * odd
+N = N[::-1]
+for _ in range(even):
+    idx = N.index("0")
+    N.pop(idx)
+
+N = N[::-1]
+
+result = "".join(N)
 print(result)
